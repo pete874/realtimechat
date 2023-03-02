9146d335bd5b2208a61863077f343699afb974e2
Petri Nurmi <pete874@hotmail.com>
2023-03-02 18:14:01 +0200

    .

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,8 +19,17 @@ const Chat = () => {
       setName(name);
       setRoom(room);
 
-      console.log(socket);
-    }, []);
+      socket.emit('join', { name, room }, () => {
+       
+      });
+
+      return () => {
+        socket.emit('disconnect')
+
+        socket.off();
+      }
+      
+    }, [ENDPOINT, location.search]);
     return (
         <h1>Chat</h1>
     )
---
+++ server/users.js
@@ -0,0 +1,22 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+    // Javascript Mastery = javascriptmastery
+
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find()
+}
+
+const removeUser = () => {
+
+}
+
+const getUser = () => {
+
+}
+
+const getUserInRoom = () => {
+
+}
